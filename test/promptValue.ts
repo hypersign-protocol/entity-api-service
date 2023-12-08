ef9a44a0469b1200679f4d37f9e62b1cb4531d82
varsha766 <varshakumari370@gmail.com>
2023-12-08 23:09:04 +0530

    updated test case based on recent chnage in hid-node as well in hs-ssi-sdk

---
+++ test/promptValue.ts
@@ -0,0 +1,23 @@
+import * as readlineSync from 'readline-sync';
+import * as fs from 'fs';
+interface EnvironmentData {
+  name: string;
+  values: { key: string; value: string; type: string }[];
+}
+
+const baseUrl: string = readlineSync.question(
+  'Enter api server Base URL with api/v1: ',
+);
+console.log(baseUrl);
+const authToken: string = readlineSync.question('Enter Api Server AuthToken: ');
+
+const environmentData: EnvironmentData = {
+  name: 'environment',
+  values: [
+    { key: 'studio-api-baseUrl', value: baseUrl, type: 'text' },
+    { key: 'access_Token', value: authToken, type: 'text' },
+  ],
+};
+
+const environmentJson: string = JSON.stringify(environmentData, null, 2);
+fs.writeFileSync('environment.json', environmentJson);
