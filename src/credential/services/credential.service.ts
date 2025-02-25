c3d646b76692dc540533192c361e6667a3804b3e
varsha766 <varshakumari370@gmail.com>
2025-02-25 10:55:35 +0530

    code pretified

--- src/credential/services/credential.service.ts
+++ src/credential/services/credential.service.ts
@@ -39,7 +39,7 @@ export class CredentialService {
     private readonly didRepositiory: DidRepository,
     private readonly txnService: TxSendModuleService,
     private readonly statusService: StatusService,
-  ) { }
+  ) {}
 
   async checkAllowence(address) {
     const url =
@@ -399,8 +399,8 @@ export class CredentialService {
       status === 'SUSPEND'
         ? 'SUSPENDED'
         : status === 'REVOKE'
-          ? 'REVOKED'
-          : 'LIVE';
+        ? 'REVOKED'
+        : 'LIVE';
     const didOfvmId = verificationMethodId.split('#')[0];
 
     const { edvId, kmsId } = appDetail;
@@ -443,8 +443,8 @@ export class CredentialService {
       const nameSpace = namespace
         ? namespace
         : this.config.get('NETWORK')
-          ? this.config.get('NETWORK')
-          : namespace;
+        ? this.config.get('NETWORK')
+        : namespace;
       if (
         verificationMethod &&
         verificationMethod.type === IKeyType.BabyJubJubKey2021
@@ -574,7 +574,7 @@ export class CredentialService {
       if (
         verifyCredentialDto.credentialDocument &&
         verifyCredentialDto.credentialDocument.proof.type ===
-        SupportedSignatureType.BJJSignature2021
+          SupportedSignatureType.BJJSignature2021
       ) {
         verificationResult = await hypersignCredential.bjjVC.verify({
           credential: verifyCredentialDto.credentialDocument as any, // will fix it latter
--- src/did/services/did.service.ts
+++ src/did/services/did.service.ts
@@ -43,7 +43,7 @@ export class DidService {
     private readonly didSSIService: DidSSIService,
     private readonly config: ConfigService,
     private readonly txnService: TxSendModuleService,
-  ) { }
+  ) {}
 
   async checkAllowence(address) {
     const url =
@@ -115,7 +115,7 @@ export class DidService {
         'options.register is true for keyType ' +
           IKeyType.EcdsaSecp256k1RecoveryMethod2020,
         IKeyType.EcdsaSecp256k1RecoveryMethod2020 +
-        ' doesnot support register without signature being passed',
+          ' doesnot support register without signature being passed',
         'options.register:false is strongly recomended',
       ]);
     }
@@ -275,7 +275,7 @@ export class DidService {
       if (!insertedDoc) {
         throw new Error(
           'Could not insert document for userCredential.walletAddress' +
-          userCredential.walletAddress,
+            userCredential.walletAddress,
         );
       }
       const { id: userKMSId } = insertedDoc;
@@ -432,7 +432,7 @@ export class DidService {
       if (!insertedDoc) {
         throw new Error(
           'Could not insert document for userCredential.walletAddress' +
-          userCredential.walletAddress,
+            userCredential.walletAddress,
         );
       }
       const { id: userKMSId } = insertedDoc;
